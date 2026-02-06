81f21816ef07e68e3b4741d6dface4f29414ae3b
psiadrian <psi.adrianrub@gmail.com>
2026-02-06 20:37:18 +0100

    movido la clase tablero a un archivo para hacer uin objeto

---
+++ tablero.py
@@ -0,0 +1,39 @@
+class tablero:
+    alto = 0
+    ancho = 0
+    cuadricula =[]
+
+    def __init__(self, alto, ancho):
+        self.alto = alto
+        self.ancho = ancho
+        for x in range(ancho):
+            self.cuadricula.append([])
+
+        for i in self.cuadricula:
+            for y in range(alto):        
+                i.append([])
+
+    #asigna el item a la casilla x,y 
+    def setCasilla(self, posX, posY, item):
+        self.cuadricula[posX][posY] = item
+        #return cuadricula
+
+    #return el item en la casilla
+    def getCasilla(self, posX, posY):
+        return self.cuadricula[posX][posY]
+
+    #llena todos los espacios del array de item
+    def fillWhitItem(self, item):
+        for x in self.cuadricula:
+            for y in range(len(self.cuadricula)):
+                self.setCasilla(x,y,item)
+
+    #devuelve la cantidad de casillas totales
+    def espacioLibre(self):
+        return len(self.cuadricula) * len(self.cuadricula[0])
+    
+    def getAncho(self):
+        return self.ancho
+    
+    def getAlto(self):
+        return self.alto
